120cf3dd8cc97b6e0066b3a9099dd7605e662b59
William <will.liiam06@gmail.com>
2021-07-04 20:56:55 +0700

    feat: add cleanse pods and instance module (#3)

---
+++ TheMovieDB/DI/Binders.swift
@@ -0,0 +1,13 @@
+//
+//  Binders.swift
+//  TheMovieDB
+//
+//  Created by William on 04/07/21.
+//  Copyright © 2021 william. All rights reserved.
+//
+
+import Foundation
+import Cleanse
+
+public typealias SingletonBinder = Binder<Singleton>
+public typealias UnscopedBinder = Binder<Unscoped>
